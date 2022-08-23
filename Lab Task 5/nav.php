6c3b93dcc3e6590696db3412a9f0b49af4f15d8c
abrarayon <106133288+abrarayon@users.noreply.github.com>
2022-08-23 21:15:40 +0600

    Add files via upload

---
+++ Lab Task 5/nav.php
@@ -0,0 +1,5 @@
+<ul>
+        <li><a href="/LAB TASK/Lab Task 5/addProduct.php"> Add Product</a></li>
+        <li><a href="/LAB TASK/Lab Task 5/showAllProducts.php"> Show all Product</a></li>
+        <li><a href="/LAB TASK/Lab Task 5/searchProduct.php"> Search Product</a></li>
+</ul>
